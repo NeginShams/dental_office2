72083c1b4efc8536a07406cb67d43f4e8ae5d4f2
NeginShams <neginshams20@gmail.com>
2020-06-17 13:56:52 +0430

    edit views

--- dent/seapp/views.py
+++ dent/seapp/views.py
@@ -74,7 +74,7 @@ class WorkTimesViewSet(viewsets.ModelViewSet):
     ordering_fields='__all__'
     permission_classes = []
 
-#@permission_classes((IsAuthenticated, ))
+"""#@permission_classes((IsAuthenticated, ))
 @permission_classes((AllowAny, ))
 class ReservationView(APIView):
     def get(self , request , format=None):
@@ -93,6 +93,32 @@ class makeReservation(APIView):
         serializer.is_valid(raise_exception=True)
         selected_time = serializer.validated_data['time']
         q=WorkTimes.objects.get(startTime=selected_time)
+        q.reserved=True
+        q.save()
+        serializer.save()
+        return Response(serializer.data , status=status.HTTP_201_CREATED)"""
+
+
+@permission_classes((AllowAny, ))
+class ReservationView(APIView):
+    def get(self , request , format=None):
+        limit_date = datetime.date.today()+ datetime.timedelta(days=30)
+        q1 = WorkTimes.objects.filter(date__gte=datetime.date.today())
+        q2 = q1.filter(date__lte=limit_date)
+        q3 = q2.exclude(reserved =True)
+        available_times = q3[:20]
+        serializer = WorkTimesSerializer(available_times , many=True)
+        return Response(serializer.data)
+
+
+@permission_classes((AllowAny, ))
+class makeReservation(APIView):
+    def post(self , request , format=None):
+        serializer = VisitSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        selected_time = serializer.validated_data['time']
+        selected_date = serializer.validated_data['date']
+        q=WorkTimes.objects.get(startTime=selected_time , date = selected_date)
         q.reserved=True
         q.save()
         serializer.save()
